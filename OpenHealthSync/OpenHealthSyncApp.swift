b5c2db4989279726e30e2e2105bda3b31c209fb4
Arden de Raaij <a.de.raaij@gmail.com>
2026-03-13 18:42:17 +0000

    Initial Commit

---
+++ OpenHealthSync/OpenHealthSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenHealthSyncApp.swift
+//  OpenHealthSync
+//
+//  Created by Arden de Raaij on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenHealthSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
